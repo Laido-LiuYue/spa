03e779dbf318dbb700dba33ec48e2a3fe53383af
Laido-LiuYue <578201842@qq.com>
2019-04-17 16:35:59 +0800

    add password0.2

--- 11-password-visual/password.js
+++ 11-password-visual/password.js
@@ -1,18 +1,32 @@
+var PasswordTextbox = function(config) {
+    var $pwd= $('<input type="password">'),
+        $pwdText = $('<input type="text">'),
+        $eye= $('<div></div>'),
+        $div= $('<div class="wd-password">密码：</div>');
 
-function(){
-    var $pwd     = $('#pwd'),
-        $pwdText = $('.pwd-text'),
-        $eye     = $('#eye');
+    $div.append($pwd);
+    $div.append($eye);
+    $div.append($pwdText);
+    $(config.container).append($div);
 
     $pwd.on('input', function(){
-        $pwdText.val($pwd.val());   
-    });
+    $pwdText.val($pwd.val());
+                    
+});
 
     $eye.mouseover(function(){
-        $pwdText.css('z-index', '10');
-    });
-
-    $eye.mouseout(function(){
-        $pwdText.css('z-index', '-10');       
-    });
+    $pwdText.css('z-index', '10');
+                    
 });
+
+     $eye.mouseout(function(){
+     $pwdText.css('z-index', '-10');
+                    
+});
+
+      this.getPwd = function() {
+        return $pwdText.val();
+                    
+      };
+
+};
